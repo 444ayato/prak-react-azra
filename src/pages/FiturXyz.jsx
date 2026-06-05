e51970f3e13eb3dd55af9971c7295975bfd0b88d
444ayato <ivana24si@mahasiswa.pcr.ac.id>
2026-06-05 14:41:08 +0700

    percobaan kedua coba coba lab

--- src/pages/FiturXyz.jsx
+++ src/pages/FiturXyz.jsx
@@ -4,7 +4,7 @@ export default function FiturXyz() {
   return (
     <div>
       <PageHeader title="Fitur Xyz" />
-      <p>Ini halaman fitur XYZ.</p>
+      <p>Ini halaman fitur XYZ. / halaman baru</p>
     </div>
   );
 }
